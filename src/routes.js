1405d9acda183c9257c5b493175eb99253b77389
pevehdev <pauloht2020@gmail.com>
2023-04-19 21:40:42 -0300

    Create: adicionando rota de deletar usuario

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ const router = Router()
 router.post('/user-create', UserController.createUser )
 router.put('/user-update/:id', UserController.updateUser)
 router.get('/user-list', UserController.listUsers)
+router.delete('/user-delete/:id', UserController.deleteUser)
 
 
 module.exports = router
