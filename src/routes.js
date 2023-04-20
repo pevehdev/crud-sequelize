93e243a46ef1053f7afe2df4b2a72bf5ed4a59b8
pevehdev <pauloht2020@gmail.com>
2023-04-19 21:27:40 -0300

    Create: adicionado rota de update e de listar os users

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const UserController = require('./controller/UserController')
 const router = Router()
 
 router.post('/user-create', UserController.createUser )
+router.put('/user-update/:id', UserController.updateUser)
+router.get('/user-list', UserController.listUsers)
 
 
 module.exports = router
